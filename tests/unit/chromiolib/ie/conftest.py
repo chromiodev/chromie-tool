96b3d334092d593d5a9838643f9afd686acd28f7
siakro <siakro@users.noreply.github.com>
2025-11-10 22:25:28 +0100

    feat: exporter saves the collection configuration

--- tests/unit/chromiolib/ie/conftest.py
+++ tests/unit/chromiolib/ie/conftest.py
@@ -7,6 +7,16 @@ from pytest_mock import AsyncMockType, MockerFixture
 def coll(mocker: MockerFixture) -> AsyncMockType:
   """Collection to use in the tests."""
 
+  # (1) create mock
   coll = mocker.AsyncMock(spec=AsyncCollection)
   type(coll).name = mocker.PropertyMock(return_value="test")
+  type(coll).configuration = mocker.PropertyMock(
+    return_value={
+      "embedding_function": (fn := mocker.Mock()),
+      "spann": {"space": "cosine"},
+    }
+  )
+  type(fn).name = mocker.Mock(return_value="default")
+
+  # (2) return mock
   return coll
